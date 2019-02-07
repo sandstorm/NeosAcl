32ce26d129a077f4a0f343eccceca00931e1f8a3
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2019-02-07 18:59:51 +0100

    Initial commit from kickstarter

---
+++ Classes/Controller/StandardController.php
@@ -0,0 +1,23 @@
+<?php
+namespace Sandstorm\NeosAcl\Controller;
+
+/*
+ * This file is part of the Sandstorm.NeosAcl package.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Controller\ActionController;
+
+class StandardController extends ActionController
+{
+
+    /**
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->view->assign('foos', array(
+            'bar', 'baz'
+        ));
+    }
+}
